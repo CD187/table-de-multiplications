239dea658cd600a04b0728a04ba2328eb8751da9
CD187 <88897520+CD187@users.noreply.github.com>
2022-07-09 17:59:39 +0200

    Update tables_class.py
    un peu plus malin

--- tables_class.py
+++ tables_class.py
@@ -12,6 +12,7 @@ class Game:
     def __init__(self):
         self.log_dir()
         self.max_trial = 30         # hard-coded value which defines number of trial in a game
+        print('\x1b[37m')
 
     def log_dir(self):
         main_dir = './table_de_multiplication'
@@ -55,7 +56,6 @@ class Game:
                         return trial
                     break
             return self.max_trial          
-
                    
 #creates counting dictionnary for each pair of operands (8 3 : (right, wrong) ...)  
 #this function return this dictionnary as attribute and return a second attribute which is a list of each primary operands.
@@ -132,7 +132,6 @@ class Game:
             except:
                 return None     #if new player : ZeroDivisionError 
 
-
     def menu_select_player(self) : raise NotImplemented    
     def menu_player(self) : raise NotImplemented            #make player select a mode : play / statistics
     def play(self) : raise NotImplemented                   #play a game
@@ -141,9 +140,8 @@ class Game:
     def wrong_answer(self): raise NotImplemented
     def right_answer(self): raise NotImplemented
 
-
 class TUI_Linux(Game):
-    def _visit_stdin(self):
+    def _visit_stdin(self):                 #thank you Nophke for this function !
         old_mode = tcgetattr(sys.stdin)
         try:
             IFLAG = 0
@@ -180,78 +178,125 @@ class TUI_Linux(Game):
         finally:
             tcsetattr(sys.stdin, TCSAFLUSH, old_mode)
  
-# _custom_input() returns a string, and takes 2 parameters : the valid chars and the way to valid the input. 
-# We can give some options prompt = 'my_string' / time = (give an integer for 'X' seconds)  / backspace = 'disable' (to disable char deletion)
-
-    def _custom_input(self,input_validation, valid_char, prompt = None, timer = None, backspace = None):
+    def _custom_input(self,input_validation, valid_char, prompt = None, timer = None, max_len = None, backspace = None, middle = None):
         if timer and timer > 0:
                     starting_time = time()
         if backspace and  backspace.lower() != 'disable':
             raise ValueError 
         char = user_input = ''
         iterator = self._visit_stdin()
-        while not input_validation(user_input):
-            if timer and time() - starting_time > timer:
-                raise ValueError 
-            char = next(iterator)
-            if valid_char(char):
-                user_input += char
-            elif char == '\x7f'  or char == '\x08':
-                if not backspace:
-                    user_input = user_input[:-1]
+        while not input_validation(user_input):            
             print('\x1b[2K', end='')
-            if prompt:
-                print(prompt, user_input, end='\r')
+            if not middle:
+                if prompt:
+                    print(prompt, user_input, end='\r')
+                else:
+                    print(user_input, end='\r')
             else:
-                print(user_input, end='\r')
+                if prompt:
+                    print(self.middle_window(prompt + user_input), end='\r')
+                else:
+                    print(self.middle_window(user_input), end='\r')
+            while True:              #this loop is done in order to avoid printing bug if we modify the terminal size 
+                if timer and time() - starting_time > timer:
+                    user_input += 'ValueError\x0d'
+                    break
+                char = next(iterator)
+                if valid_char(char):
+                    if not max_len or max_len and len(user_input) < max_len:
+                        user_input += char
+                        break
+                elif char == '\x7f'  or char == '\x08':
+                    if not backspace:
+                        user_input = user_input[:-1]
+                        break
         return user_input
 
-    def menu_select_player(self):
-        os.system('clear')
-        sys.stdout.write('\x1b[?25l') #hide cursor 
-        print('choix du joueur :','\n')
-        for player in self.players:
-            print('\x1b[32m' + player + '\x1b[37m')
-        print('\necris \x1b[32mnouveau\x1b[37m pour créer un nouveau joueur, sinon ecris ton pseudo :\n')
-        player_name = self._custom_input(lambda user_input : user_input in [*self.players, 'nouveau'],
-                                         lambda char : char in [*ascii_letters, *digits],
-                                         prompt='pseudo :')
-        if player_name == 'nouveau':
+    def menu_selector(self, list_of_string, size, prefixe, suffixe, prompt = None):
+        i = 0
+        keys = ['\x1b\x5b\x41','\x1b\x5b\x42', '\x0d']
+        validation = False
+        while True:
             os.system('clear')
-            while player_name == 'nouveau' or player_name == '':
-                player_name = self._custom_input(lambda user_input : True if user_input.endswith('\x0d') else False,
-                                                 lambda char : True if char in [*ascii_letters, *digits, '\x0d'] else False,
-                                                 prompt = 'pseudo :')
+            if prompt:
+                print('\n', self.middle_window(prompt),'\n')
+            max_lenght = len(list_of_string) 
+            if i == max_lenght :
+                i = 0
+            elif i == -1:
+                i = max_lenght - 1
+            for index,element in enumerate(list_of_string):
+                if index == i :
+                    print(self.middle_window(element,size = size, prefixe = prefixe, suffixe = suffixe)) 
+                else:
+                    print(self.middle_window(element))
+            while validation == False:
+                char = self._custom_input(lambda ui : True if ui != '' else False,
+                                          lambda char : True if char in keys else False)
+                if char == '\x1b\x5b\x42':    #arrow down
+                    i += 1
+                    break
+                elif char == '\x1b\x5b\x41':  #arrow up
+                    i -= 1
+                    break
+                elif char == '\x0d':          #enter
+                    validation = True
+                    break
+            if validation == True:
+                choice = list_of_string[i]
+                break               
+        return choice
+    
+    def middle_window(self, string, size = None, prefixe = None, suffixe = None):
+        col, lin = os.get_terminal_size()
+        spaces = round((col - len(string)) / 2)
+        if size and prefixe and suffixe:
+            try:
+                a = size - len(string)
+                if a < 0:
+                    raise ValueError
+            except:
+                pass
+            colored_spaces = spaces - round((col-size) / 2)
+            new_string = (spaces - colored_spaces) * ' ' + prefixe + colored_spaces * ' '+string + (a - colored_spaces) * ' ' + suffixe
+        else:
+            new_string = spaces * ' ' + string
+        return new_string
+
+    def menu_select_player(self):
+        sys.stdout.write('\x1b[?25l')                   #hide cursor 
+        max_len_player_name = 16
+        if len(self.players) < 9:                       #max 10 players
+            self.players.append('nouveau joueur')
+        player_name = self.menu_selector(self.players, 24, '\x1b[30;107m', '\x1b[0;1m', prompt = 'choix du joueur :')
+        if player_name == 'nouveau joueur':
+            os.system('clear')
+            print(self.middle_window('entre ton pseudo :'), '\n\n\n')
+            player_name = self._custom_input(lambda user_input : True if user_input.endswith('\x0d') else False,
+                                             lambda char : True if char in [*ascii_letters, *digits, '\x0d'] else False,
+                                             max_len = 16,
+                                             middle = True)
             player_name = player_name[:-1]
             with open(self.path_log+f'/{player_name}', 'a') as file:
                 file.write(player_name + '\n')
+                self.players[-1] = player_name    
+        else:
+            self.players = self.players[:-1]
         self.player_name = player_name
 
     def menu_player(self):
         os.system('clear')
         welcome = f'bienvenue {self.player_name}' 
-        i = 0 
-        while i != len(welcome) + 1:
-            print('\x1b[2K', end='')
-            print(welcome[:i], end ='\r')
-            i += 1
-            sleep(0.05)
-        sleep(1)
-        print('\n\n\n')
-        print('Pour voir tes statistique appuie sur la touche ---> S')
-        print('pour jouer appuie sur la touche ---> entrée')
-        print('pour changer de joueur appuie sur la touche ---> retour')
-        print("\n\n\nA n'importe quel moment tu peux quitter en appuyant sur ---> ctrl + C")
-        valid_touch = ['\x0d' , 's' , 'S' , '\x7f' , '\x08']
-        selection = self._custom_input(lambda user_input : True if user_input else False,
-                                       lambda char : True if char in valid_touch else False)
-        if selection == '\x0d':
-            return 1
-        elif selection == 's' or selection =='S':
+        menu = ['statistiques', 'jouer', 'changer de joueur']
+        selection = self.menu_selector(menu, 24, '\x1b[30;107m', '\x1b[0;1m', prompt = welcome + '\n')
+        print("\n\n\nA n'importe quel moment de la partie tu peux quitter en appuyant sur -> ctrl + C")
+        if selection == 'statistiques':
             return 0
-        elif selection == '\x7f' or selection == '\x08':
+        elif selection == 'jouer':
+            return 1
+        elif selection == 'changer de joueur':
             return -1
-    
+
     def player_statistics(self):
         os.system('clear')
         self.dict_maker()
@@ -271,26 +316,27 @@ class TUI_Linux(Game):
         if b: 
             A = colorized_average(a)
             B = colorized_average(b)
-            print(f"ta moyenne generale est de : {A}\n\n") 
-            print(f"le score de ta dernière partie est : {B}\n\n") 
-            print(f"Ta moyenne pour chaque table est de :\n")
+            print(self.middle_window(f"ta moyenne generale est de : {A}")) 
+            print(self.middle_window(f"le score de ta dernière partie est : {B}\n\n")) 
+            print(self.middle_window(f"Ta moyenne pour chaque table est de :\n"))
             for k,v in self.by_op_average.items():
                 V = colorized_average(v)
-                print(    f"Table de {k} : {V}")
+                print(self.middle_window(f"Table de {k} : {V}"))
             weakness = self.weakness()
             if weakness == None:
-                print("Bravo ! pour le moment c'est un sans faute !\n\n") 
+                print('\n\n' + self.middle_window("Bravo ! pour le moment c'est un sans faute !") +'\n\n') 
             elif len(weakness) == 1:
                 x = ''.join(weakness)
-                print(f"\n\nil va falloir réviser ta table de {x}\n\n")
+                print('\n\n' + self.middle_window(f'il va falloir réviser ta table de {x}') + '\n\n')
             else:
                 x = ', '.join(weakness[:-1]) + ' et ' + str(weakness[-1])
-                print(f"\n\nil va falloir réviser tes tables de {x}\n\n")
+                print('\n\n' + self.middle_window(f"il va falloir réviser tes tables de {x}") + '\n\n')
         else:
-            print('aucune moyenne ne peut être établie pour le moment, joue au moins une partie complète.\n\n')
+            print(self.middle_window('aucune moyenne ne peut être établie pour le moment, joue au moins une partie complète.') + '\n\n')
         back = self._custom_input(lambda prop : True if prop.startswith('\x0d') else False,
                                   lambda char : True if char == '\x0d' else False,
-                                  prompt = 'Appuie sur entrée pour revenir au menu précédent')
+                                  prompt = 'Appuie sur entrée pour revenir au menu précédent',
+                                  middle = True)
 
     def play(self):
         os.system('clear')
@@ -308,23 +354,21 @@ class TUI_Linux(Game):
                     x, y, result = self.random_question()           #
 
                 no_value = True
-                try:
-                    while no_value == True:
-                        user_prop = self._custom_input(lambda  prop : True if prop.endswith('\x0d') else False,
-                                                       lambda char : True if char in [*digits, '\x0d']  else False,
-                                                       prompt = f'\x1b[32m{trial}\x1b[37m    {x} x {y} =', timer = 10)   
-                        if user_prop != '\x0d':
-                            no_value = False
-                    user_prop = user_prop[:-1]
-                    if user_prop == '':
-                        self.wrong_answer(x, y, result)
-                    elif int(user_prop) != int(result):
-                        self.wrong_answer(x, y, result)
-                    else :
-                        self.right_answer()
-                except ValueError:
+                while no_value == True:
+                    user_prop = self._custom_input(lambda  prop : True if prop.endswith('\x0d') else False,
+                                                   lambda char : True if char in [*digits, '\x0d']  else False,
+                                                   prompt = f'\x1b[32m{trial}\x1b[37m    {x} x {y} =', 
+                                                   timer = 10)   
+                    if user_prop != '\x0d':
+                        no_value = False
+                user_prop = user_prop[:-1]
+                if user_prop == 'ValueError':
                     self.timeout(x, y, result)
                     user_prop = 'TIMEOUT'
+                elif user_prop == '' or int(user_prop) != int(result):
+                    self.wrong_answer(x, y, result)
+                else:
+                    self.right_answer()
                 trial -= 1 
                 values = f'{trial} {x} {y} {result} {user_prop}'
                 file.write('\n' + values)  
